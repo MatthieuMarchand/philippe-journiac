a8528b740f498c78b0dbfbf825ccf01f0e1e8696
Matthieu <matthieumarchand.pro@gmail.com>
2026-04-03 16:45:26 +0200

    Backup 03/04/2026

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ export default defineConfig({
       preprocessorOptions: {
         scss: {
           additionalData: `
-            @use "/src/assets/styles/_reset.scss";
             @use "/src/assets/styles/_variables.scss" as *;
           `,
         },
