5988f13a34fe1d81d5e35e690c41f77f31481296
Loic Leforestier <loicleforestier.pro@gmail.com>
2026-06-08 20:48:27 +0200

    added table of content

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,30 +1,40 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const tableaux = defineCollection({
-    loader: glob({ base: './src/content/tableaux', pattern: '**/*.md' }),
-    schema: z.object({
-        title: z.string(),
-        image: z.string(),
-        dimensions: z.string(),
-        for_sale: z.boolean(),
-        price: z.number(),
-        date_created: z.date(),
-    }),
+  loader: glob({ base: "./src/content/tableaux", pattern: "**/*.md" }),
+  schema: z.object({
+    title: z.string(),
+    image: z.string(),
+    dimensions: z.string(),
+    for_sale: z.boolean(),
+    price: z.number(),
+    date_created: z.date(),
+  }),
 });
 
 const actualites = defineCollection({
-    loader: glob({ base: './src/content/actualites', pattern: '**/*.md' }),
-    schema: z.object({
-        title: z.string(),
-        lede: z.string(),
-        image: z.string(),
-        publish_at: z.date(),
-    }),
+  loader: glob({ base: "./src/content/actualites", pattern: "**/*.md" }),
+  schema: z.object({
+    title: z.string(),
+    lede: z.string(),
+    image: z.string(),
+    publish_at: z.date(),
+  }),
+});
+
+const distinctions = defineCollection({
+  loader: glob({ base: "./src/content/distinctions", pattern: "**/*.md" }),
+  schema: z.object({
+    type: z.string(),
+    location: z.string(),
+    received_at: z.date(),
+  }),
 });
 
 export const collections = {
-    actualites: actualites,
-    tableaux: tableaux
+  actualites: actualites,
+  tableaux: tableaux,
+  distinctions: distinctions,
 };
